0eb13bd7fea3315f12a07b5c49d372f74053a02f
gilcu3 <828241+gilcu3@users.noreply.github.com>
2020-04-25 01:10:04 +0200

    added support for basic commands, new version

--- src/extension.ts
+++ src/extension.ts
@@ -1,24 +1,67 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
-
-// this method is called when your extension is activated
-// your extension is activated the very first time the command is executed
+import * as child_process from 'child_process';
 export function activate(context: vscode.ExtensionContext) {
 
-    // Use the console to output diagnostic information (console.log) and errors (console.error)
-    // This line of code will only be executed once when your extension is activated
-    console.log('Congratulations, your extension "my-first-extension" is now active!');
-
-    // The command has been defined in the package.json file
-    // Now provide the implementation of the command with  registerCommand
-    // The commandId parameter must match the command field in package.json
-    var disposable = vscode.commands.registerCommand('extension.sayHello', () => {
-        // The code you place here will be executed every time your command is executed
-
-        // Display a message box to the user
-        vscode.window.showInformationMessage('Hello World!');
+    console.log('Tamarin extension is now active');
+    let stderrOut = vscode.window.createOutputChannel("stderrTamarin");
+    let terminal = vscode.window.createTerminal("serverTamarin");
+    
+    var checkSyntaxCommand = vscode.commands.registerCommand('tamarin.checkSyntax', () => {
+        if (vscode.window.activeTextEditor){
+            let source = vscode.window.activeTextEditor.document.uri.fsPath;
+            let program = "tamarin-prover";
+            let args = ["--parse-only", source];
+            let result = child_process.spawnSync(program, args);
+            if (result.status !== 0) {
+                if (result.stderr.length > 0) {
+                        let escaped_output = result.stderr.toString();
+                        stderrOut.append(escaped_output);
+                        stderrOut.show();
+                    }
+                vscode.window.showErrorMessage("Syntax errors detected");
+            }
+            else {
+                stderrOut.append("No error detected\n");
+                vscode.window.showInformationMessage("Syntax is correct");
+            }
+        }
+        
     });
 
-    context.subscriptions.push(disposable);
+    var checkSemanticsCommand = vscode.commands.registerCommand('tamarin.checkSemantics', () => {
+        if (vscode.window.activeTextEditor){
+            let source = vscode.window.activeTextEditor.document.uri.fsPath;
+            let program = "tamarin-prover";
+            let args = ["--quit-on-warning", source];
+            let result = child_process.spawnSync(program, args);
+            if (result.status !== 0) {
+                if (result.stderr.length > 0) {
+                        let escaped_output = result.stderr.toString();
+                        stderrOut.append(escaped_output);
+                        stderrOut.show();
+                    }
+                vscode.window.showErrorMessage("Semantic errors detected.");
+            }
+            else {
+                stderrOut.append("No error detected\n");
+                vscode.window.showInformationMessage("Semantics is correct");
+            }
+        }
+        
+    });
+
+    var runServerCommand = vscode.commands.registerCommand('tamarin.runServer', () => {
+        if (vscode.window.activeTextEditor){
+            let source = vscode.window.activeTextEditor.document.uri.fsPath;
+            let program = "tamarin-prover";
+            let args = ["interactive", "--quit-on-warning", source];
+            terminal.sendText(program + " " + args.join(' '), true);
+            terminal.show();
+        }
+        
+    });
+
+    context.subscriptions.push(checkSyntaxCommand);
+    context.subscriptions.push(checkSemanticsCommand);
+    context.subscriptions.push(runServerCommand);
 }
