72ad8ef41ee19f799858b17edddccbb2ae756048
gilcu3 <828241+gilcu3@users.noreply.github.com>
2020-04-25 02:05:00 +0200

    added autosave to commands involving execution of code

--- TODO.md
+++ TODO.md
@@ -1,7 +1,9 @@
 # TODO
 
 - [x] ==> for implication
-- [o] autoclose <> (maybe can't be done, would need to distinguish between pair and comparison)
+- ~~[x] autoclose <> (maybe can't be done, would need to distinguish between pair and comparison)~~
 - [x] fix functions that overwrite pattern matching inside
 - [x] add restriction name support
 - [x] add lemma annotations
+- [x] save current file before executing commands that affect the code
+- [x] when exiting terminal after execution an error message appears [terminal.integrated.showExitAlert option fixes it]
--- src/extension.ts
+++ src/extension.ts
@@ -1,5 +1,6 @@
 import * as vscode from 'vscode';
 import * as child_process from 'child_process';
+
 export function activate(context: vscode.ExtensionContext) {
 
     console.log('Tamarin extension is now active');
@@ -9,21 +10,26 @@ export function activate(context: vscode.ExtensionContext) {
     var checkSyntaxCommand = vscode.commands.registerCommand('tamarin.checkSyntax', () => {
         if (vscode.window.activeTextEditor){
             let source = vscode.window.activeTextEditor.document.uri.fsPath;
-            let program = "tamarin-prover";
-            let args = ["--parse-only", source];
-            let result = child_process.spawnSync(program, args);
-            if (result.status !== 0) {
-                if (result.stderr.length > 0) {
-                        let escaped_output = result.stderr.toString();
-                        stderrOut.append(escaped_output);
-                        stderrOut.show();
+            vscode.window.activeTextEditor.document.save().then( () => {
+                    let program = "tamarin-prover";
+                    let args = ["--parse-only", source];
+                    let result = child_process.spawnSync(program, args);
+                    if (result.status !== 0) {
+                        if (result.stderr.length > 0) {
+                                let escaped_output = result.stderr.toString();
+                                stderrOut.append(escaped_output);
+                                stderrOut.show(true);
+                            }
+                        vscode.window.showErrorMessage("Syntax errors detected");
                     }
-                vscode.window.showErrorMessage("Syntax errors detected");
-            }
-            else {
-                stderrOut.append("No error detected\n");
-                vscode.window.showInformationMessage("Syntax is correct");
-            }
+                    else {
+                        stderrOut.append("No error detected\n");
+                        vscode.window.showInformationMessage("Syntax is correct");
+                    }
+                },() => {
+                    vscode.window.showErrorMessage("Error saving source");
+                });
+            
         }
         
     });
@@ -31,21 +37,25 @@ export function activate(context: vscode.ExtensionContext) {
     var checkSemanticsCommand = vscode.commands.registerCommand('tamarin.checkSemantics', () => {
         if (vscode.window.activeTextEditor){
             let source = vscode.window.activeTextEditor.document.uri.fsPath;
-            let program = "tamarin-prover";
-            let args = ["--quit-on-warning", source];
-            let result = child_process.spawnSync(program, args);
-            if (result.status !== 0) {
-                if (result.stderr.length > 0) {
-                        let escaped_output = result.stderr.toString();
-                        stderrOut.append(escaped_output);
-                        stderrOut.show();
-                    }
-                vscode.window.showErrorMessage("Semantic errors detected.");
-            }
-            else {
-                stderrOut.append("No error detected\n");
-                vscode.window.showInformationMessage("Semantics is correct");
-            }
+            vscode.window.activeTextEditor.document.save().then( () => {
+                let program = "tamarin-prover";
+                let args = ["--quit-on-warning", source];
+                let result = child_process.spawnSync(program, args);
+                if (result.status !== 0) {
+                    if (result.stderr.length > 0) {
+                            let escaped_output = result.stderr.toString();
+                            stderrOut.append(escaped_output);
+                            stderrOut.show(true);
+                        }
+                    vscode.window.showErrorMessage("Semantic errors detected.");
+                }
+                else {
+                    stderrOut.append("No error detected\n");
+                    vscode.window.showInformationMessage("Semantics is correct");
+                }
+            }, () => {
+                vscode.window.showErrorMessage("Error saving source");
+            });
         }
         
     });
@@ -53,10 +63,14 @@ export function activate(context: vscode.ExtensionContext) {
     var runServerCommand = vscode.commands.registerCommand('tamarin.runServer', () => {
         if (vscode.window.activeTextEditor){
             let source = vscode.window.activeTextEditor.document.uri.fsPath;
-            let program = "tamarin-prover";
-            let args = ["interactive", "--quit-on-warning", source];
-            terminal.sendText(program + " " + args.join(' '), true);
-            terminal.show();
+            vscode.window.activeTextEditor.document.save().then( () => {
+                let program = "tamarin-prover";
+                let args = ["interactive", "--quit-on-warning", source];
+                terminal.sendText(program + " " + args.join(' '), true);
+                terminal.show();
+            }, () => {
+                vscode.window.showErrorMessage("Error saving source");
+            });
         }
         
     });
