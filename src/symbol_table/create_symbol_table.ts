8014200a79bbb95c3522c8970fb66b7b05fcf29c
Thom Wiggers <thom@thomwiggers.nl>
2024-10-17 13:36:15 +0200

    Add true symbols to signatures

--- src/symbol_table/create_symbol_table.ts
+++ src/symbol_table/create_symbol_table.ts
@@ -224,8 +224,8 @@ const AssociatedFunctions: string[][] =
 ['h', '1'],
 ['sdec', '2', 'senc', '2'],
 ['aenc', '2', 'adec', '2', 'pk', '1'],
-['sign', '2', 'verify', '3', 'pk', '1'],
-['revealSign', '2', 'revealVerify', '3', 'getMessage', '1', 'pk', '1'],
+['sign', '2', 'verify', '3', 'pk', '1', 'true', 0],
+['revealSign', '2', 'revealVerify', '3', 'getMessage', '1', 'pk', '1', 'true', 0],
 ['pmult', '2', 'em', '2'],
 ['XOR', '2', 'zero', '0'],
 ['fst', '1', 'snd', '1', 'pair', '2']
