7ebf3f2024ef32f2f72b9aed1d36d8bdadffda82
e0297u <zoe.le-gleut@telecomnancy.eu>
2025-07-02 13:24:31 +0200

    + run_shorcut

--- client/src/extension.ts
+++ client/src/extension.ts
@@ -1,9 +1,11 @@
 import { ExtensionContext } from 'vscode';
 import { startLanguageServer, stopLanguageServer } from './languageServer';
+import {runShortcut} from './run_shortcut';
 
 export async function activate(context: ExtensionContext) {
     console.log('[Client] Activating Tamarin extension...');
     await startLanguageServer(context);
+    runShortcut(context);
 }
 
 export async function deactivate() {
--- client/src/run_shortcut.ts
+++ client/src/run_shortcut.ts
@@ -1,59 +1,64 @@
-//TODO : need to be updated to use client lsp 
 
 import * as vscode from 'vscode';
 import path = require('path');
 
 
 export function runShortcut(context : vscode.ExtensionContext){
+  let playButton : vscode.StatusBarItem = vscode.window.createStatusBarItem(vscode.StatusBarAlignment.Right, 100);
+  playButton.text = "$(triangle-right) Run";
+  playButton.command = "tamarin.showActions";
+  playButton.show();
+  const showActions = vscode.commands.registerCommand('tamarin.showActions', async () => {
+      const actions: vscode.QuickPickItem[] = [
+        { label: 'Run Tamarin Prover', description: 'Run Tamarin prover ' },
+        { label: 'Run Tamarin Prover  auto-sources', description: 'Run Tamarin prover with --auto-sources option'  },
+      ];
 
-//Feature to run tamarin directly using vscode just click on the bottom right handcorner and select the mode.
+      const selectedAction = await vscode.window.showQuickPick(actions, {
+        placeHolder: 'Select an action',
+      });
 
-let playButton : vscode.StatusBarItem = vscode.window.createStatusBarItem(vscode.StatusBarAlignment.Right, 100);
-playButton.text = "$(triangle-right) Run";
-playButton.command = "tamarin.showActions";
-playButton.show();
-
-const showActions = vscode.commands.registerCommand('tamarin.showActions', async () => {
-    const actions: vscode.QuickPickItem[] = [
-      { label: 'Run Tamarin Prover', description: 'Run Tamarin prover ' },
-      { label: 'Run Tamarin Prover  auto-sources', description: 'Run Tamarin prover with --auto-sources option'  },
-    ];
-
-    const selectedAction = await vscode.window.showQuickPick(actions, {
-      placeHolder: 'Select an action',
+      if (selectedAction && selectedAction.label === 'Run Tamarin Prover') {
+        vscode.commands.executeCommand('tamarin.play')
+      }
+      else if (selectedAction && selectedAction.label === 'Run Tamarin Prover  auto-sources'){
+          vscode.commands.executeCommand('tamarin.play2')
+      }
     });
 
-    if (selectedAction && selectedAction.label === 'Run Tamarin Prover') {
-      vscode.commands.executeCommand('tamarin.play')
-    }
-    else if (selectedAction && selectedAction.label === 'Run Tamarin Prover  auto-sources'){
-        vscode.commands.executeCommand('tamarin.play2')
-    }
+  const runTamarin = vscode.commands.registerCommand('tamarin.play', async () => {
+      const activeEditor = vscode.window.activeTextEditor;
+      if(activeEditor){
+          await activeEditor.document.save();
+          const document = activeEditor.document;
+          const filePath = document.uri.fsPath;
+          const fileDirectory = path.dirname(filePath);
+          const terminal = vscode.window.createTerminal({
+              name: `Play: Tamarin`,
+              cwd: fileDirectory
+          });
+          terminal.sendText(`tamarin-prover interactive "${filePath}"`);
+          terminal.show();
+      }
   });
 
-const runTamarin = vscode.commands.registerCommand('tamarin.play', () => {
-    if(vscode.window.activeTextEditor){
-        const source = vscode.window.activeTextEditor.document.uri.fsPath;
-        const program = "tamarin-prover interactive "; 
-        const args = [path.basename(source)];
-        const terminal = vscode.window.createTerminal(`Play: Tamarin`);
-        terminal.sendText(`${program} ./${args.join(" ")}`);
-        terminal.show();
-    }
-});
-
-    const runTamarinAutoSources = vscode.commands.registerCommand('tamarin.play2', () => {
-        if(vscode.window.activeTextEditor){
-            const source = vscode.window.activeTextEditor.document.uri.fsPath;
-            const program = "tamarin-prover interactive "; 
-            const args = [path.basename(source)];
-            const terminal = vscode.window.createTerminal(`Play: Tamarin`);
-            terminal.sendText(`${program} ./${args.join("")} --auto-sources`);
+      const runTamarinAutoSources = vscode.commands.registerCommand('tamarin.play2', async () => {
+          const activeEditor = vscode.window.activeTextEditor;
+          if(activeEditor){
+            await activeEditor.document.save();
+            const document = activeEditor.document;
+            const filePath = document.uri.fsPath;
+            const fileDirectory = path.dirname(filePath)
+            const terminal = vscode.window.createTerminal({
+                name: `Play: Tamarin`,
+                cwd: fileDirectory
+            });
+            terminal.sendText(`tamarin-prover interactive "${filePath}" --auto-sources`);
             terminal.show();
-        }
+          }
 
-});
-
-context.subscriptions.push(runTamarin);
-context.subscriptions.push(runTamarinAutoSources);
-}
+  });
+  context.subscriptions.push(showActions);
+  context.subscriptions.push(runTamarin);
+  context.subscriptions.push(runTamarinAutoSources);
+  }
